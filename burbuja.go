029f8f1d43d9f3cbe8dbd96e8e90a6490a457ddf
Jahasiel Serna <jh.serna.b@alumnos.udg.com>
2020-10-20 10:01:52 -0500

    Función Burbuja() implementada

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,27 @@
 package main
 
-func Burbuja(s []int64)  {
+import "fmt"
 
+func Burbuja(s []int64)  {
+	swipe := func(s []int64, a, b int) {
+		t := s[a]
+		s[a] = s[b]
+		s[b] = t
+	}
+	limit := len(s) - 1
+
+	for limit > 0 {
+		for i := 0; i < limit; i++ {
+			if s[i] > s[i + 1] {
+				swipe(s, i, i + 1)
+			}
+		}
+		limit--
+	}
 }
 
 func main()  {
-	
+	s := []int64{8, 4, 9, 1, 7, 3, 0, 6, 5, 2}
+	Burbuja(s)
+	fmt.Println(s)
 }
